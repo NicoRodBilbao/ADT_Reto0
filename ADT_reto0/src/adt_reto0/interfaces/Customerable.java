c4ebd27b1f42724552f432990401199ffe219495
Nikolás Rodríguez <2002nikoniko2002@gmail.com>
2022-09-14 13:35:54 +0200

    Starting out the project, basic classes and structure

---
+++ ADT_reto0/src/adt_reto0/interfaces/Customerable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adt_reto0.interfaces;
+
+/**
+ *
+ * @author nikol
+ */
+public interface Customerable {
+    
+}
