f3c9839fcc4e4814104103bf7436851ed4f03255
Nikolás Rodríguez <2002nikoniko2002@gmail.com>
2022-09-20 10:12:00 +0200

    Started tests for customer

---
+++ ADT_reto0/test/test_data_access/TestDACustomer.java
@@ -0,0 +1,37 @@
+package test_data_access;
+
+import static org.junit.Assert.*;
+import adt_reto0.dataAccess.DAAccount;
+import adt_reto0.dataAccess.DACustomer;
+import adt_reto0.classes.Customer;
+import adt_reto0.classes.Account;
+
+public class TestDACustomer {
+    
+    private Customer testCustomer;
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testCreateClient() {
+        //TODO
+    }
+    
+    @Test
+    public void testGetCustomerData() {
+        //TODO
+    }
+    
+    @Test
+    public void testGetAccountData() {
+        //TODO
+    }
+    
+    @After
+    public void tearDown() {
+        //TODO
+    }
+    
+}
