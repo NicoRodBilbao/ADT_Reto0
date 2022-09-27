093d77e74e52136edea07267a285ffc70ea5b695
joana-renteria <joana.rente6@tuta.io>
2022-09-27 09:15:45 +0200

    continue implementing the menu class

--- ADT_reto0/src/adt_reto0/Menu.java
+++ ADT_reto0/src/adt_reto0/Menu.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import adt_reto0.factory.*;
+
 public class Menu {
 
 	private Scanner console = new Scanner(System.in);
@@ -58,11 +60,10 @@ public class Menu {
 	
 	private int[] getParams(String s) {
 		System.out.print("\nIntroduce the " + s + ": ");
-		String input = console.next();
-		//return new ArrayList<String>(List.of(input.split(" "))).stream()
-			//.map(p -> p.parseInt());
-		int[] result = {0};
-		return result;
+		String[] input = console.next().split(" ");
+		return Arrays.stream(input)
+			.mapToInt(p -> Integer.parseInt(p))
+			.toArray();
 	}
 
 	private void menuManager() {
@@ -91,6 +92,7 @@ public class Menu {
 
 	private void customerManager() {
 		int option = -1;
+		int[] param = {-1};
 		while(option != 0) {
 			this.greeter("Managing customers");
 			option = this.optionPrinter(customerOptions);
@@ -98,12 +100,16 @@ public class Menu {
 				case 0:
 					break;
 				case 1:
-					int param = getParams("Customer Id")[0];
-					CustomerDAFactory.getAccessCustomer().createCustomer(param);
+					param = getParams("Customer Id");
+					CustomerDAFactory.getAccessCustomer().createCustomer(param[0]);
 					break;
 				case 2:
+					param = getParams("Customer Id");
+					CustomerDAFactory.getAccessCustomer().getCustomerData(param[0]);
 					break;
 				case 3:
+					param = getParams("Customer Id");
+					CustomerDAFactory.getAccessCustomer().listCustomerAccounts(param[0]);
 					break;
 				default:
 					System.out.println("Unknown option!");
@@ -114,6 +120,7 @@ public class Menu {
 
 	private void accountManager() {
 		int option = -1;
+		int[] param = {-1};
 		while(option != 0) {
 			this.greeter("Managing accounts");
 			option = this.optionPrinter(accountOptions);
@@ -121,10 +128,16 @@ public class Menu {
 				case 0:
 					break;
 				case 1:
+					param = getParams("Customer Id");
+					AccountADFactory.getAccessAccount().createAccount(param[0]);
 					break;
 				case 2:
+					param = getParams("Customer Id && Account Id");
+					AccountADFactory.getAccessAccount().addClientToAccount(param[0], param[1]);
 					break;
 				case 3:
+					param = getParams("Account Id");
+					AccountADFactory.getAccessAccount().getAccountData(param[0]);
 					break;
 				default:
 					System.out.println("Unknown option!");
@@ -135,6 +148,7 @@ public class Menu {
 
 	private void movementManager() {
 		int option = -1;
+		int[] param = {-1};
 		while(option != 0) {
 			this.greeter("Managing movements");
 			option = this.optionPrinter(movementOptions);
@@ -142,10 +156,12 @@ public class Menu {
 				case 0:
 					break;
 				case 1:
+					param = getParams("Source Account && Destination Account && Ammount");
+					MovementADFactory.getAccessMovement().registerMovement(param[0], param[1], param[2]);
 					break;
 				case 2:
-					break;
-				case 3:
+					param = getParams("Account Id");
+					MovementADFactory.getAccessMovement().getAccountMovements(param[0]);
 					break;
 				default:
 					System.out.println("Unknown option!");
