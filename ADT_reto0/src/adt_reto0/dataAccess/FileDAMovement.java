65e5e4aae93f218c2f3284d58fe3d313fa5b30dd
joana-renteria <joana.rente6@tuta.io>
2022-10-02 23:54:27 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- ADT_reto0/src/adt_reto0/dataAccess/FileDAMovement.java
+++ ADT_reto0/src/adt_reto0/dataAccess/FileDAMovement.java
@@ -6,18 +6,25 @@ import adt_reto0.classes.Movement;
 import adt_reto0.interfaces.Movementable;
 import java.util.Date;
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class FileDAMovement implements Movementable {
 
     private FileManager<Movement> fmMovement = new FileManager<>(Movement.class);
     private FileManager<Account> fmAccount = new FileManager<>(Account.class);
     
-    public void registerMovement(Integer id, Double amount, Double balance, String description, Date timestamp, Double destination) {
+    public void registerMovement(Double destination, Double amount) {
+        String description = amount > 0 ? "Desposit" : "Payment";
+        Date timestamp = new Date();
+        Integer id = fmMovement.getLastMatch(m -> !m.getId().equals(1)).getId();
+        Double balance = fmMovement.getLastMatch(m -> m.getId().equals(id)).getBalance() + amount;
+        
         Movement m = new Movement(id, amount, balance, description, timestamp, destination);
         fmMovement.addObject(m);
     }
-    
+                    
     public ArrayList<Movement> getAccountMovements(Double MovementId) {
+        //TODO
         return null;
     }
     
